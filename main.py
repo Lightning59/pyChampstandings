9400000393cbf6dc509baa4a1b43bbb7e3c4e4f5
Evan McCaleb <evanmccaleb@gmail.com>
2022-05-08 14:21:32 -0400

    format_outsheet now calculates and selects the appropriate field of cells

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ file then update that file with pretty output of championship standings"""
 import tkinter
 import tkinter.filedialog
 import openpyxl
+import openpyxl.worksheet
+import openpyxl.utils
 
 
 def get_wb():
@@ -34,11 +36,13 @@ class champWB(object):
         self.summarysheets = []
         self.summarystr=summarystr
         self.othersheets = []
+        self.series=[]
         lraw = len(rawstr)
         lsummary = len(summarystr)
         for sheet in wb.sheetnames:
             if sheet[:lraw]==rawstr:
                 self.rawdatasheets.append(sheet)
+                self.series.append(sheet[lraw:])
             elif sheet[:lsummary]==summarystr:
                 self.summarysheets.append(sheet)
             else:
@@ -51,10 +55,32 @@ class champWB(object):
 
 
     def init_outsheets(self):
+        '''create new blank summary sheets for each series.'''
         for sheet in self.rawdatasheets:
             if sheet not in self.summarysheets:
                 self.wb.create_sheet(sheet.replace(self.rawstr,self.summarystr))
                 self.summarysheets.append(sheet)
+
+    def format_outsheet(self,sheet:str,numweeks:int,numracers:int):
+
+        #3 lines worth of headers
+        output_height=3+numracers
+        #2X num weeks covers driver and pts column each week then need to add positions
+        output_width=2*numweeks
+        for i in range(numweeks):
+            output_width+=i+1
+        ws=self.wb.get_sheet_by_name(sheet)
+        endletter=openpyxl.utils.cell.get_column_letter(output_width)
+        endcell=endletter+str(output_height)
+        graphic_range=ws['A1':endcell]
+
+
+        print('test')
+
+
+
+
+
 
 
     def writeout(self):
@@ -69,4 +95,5 @@ if __name__ == '__main__':
 
     the_wb = champWB(get_wb(),RAWNAMEBASE, SUMMARYNAMEBAE)
     the_wb.init_outsheets()
+    the_wb.format_outsheet("rawdata_Clio",2,5)
     the_wb.writeout()
