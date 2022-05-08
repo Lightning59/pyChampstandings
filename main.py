851834a2425414b5da45a22225e473beac3164d1
Evan McCaleb <evanmccaleb@gmail.com>
2022-05-08 16:18:42 -0400

    Format all borders correctly.

--- main.py
+++ main.py
@@ -73,6 +73,7 @@ class champWB(object):
         ws=self.wb[sheet]
         ws.merge_cells(start_row=1,start_column=1,end_row=1,end_column=output_width)
 
+
         startweekcol=1
         for i in range(1,numweeks+1):
             ws.merge_cells(start_row=2,start_column=startweekcol,end_row=2,end_column=startweekcol+i+1)
@@ -89,11 +90,62 @@ class champWB(object):
                 cell.fill=whiteFill
 
         thickblack=openpyxl.styles.Side(border_style='medium',color="000000")
+        thinblack = openpyxl.styles.Side(border_style='thin', color="000000")
         thick_allsides_border=openpyxl.styles.Border(left=thickblack, right=thickblack, top= thickblack, bottom= thickblack)
+        # First Two Headers for series and weeks - Set Borders
         for cell in graphic_range[0]:
             cell.border=thick_allsides_border
         for cell in graphic_range[1]:
             cell.border = thick_allsides_border
+
+        #Cacluate indicies for week breaks
+        i_week_breaks=[]
+        prevstop=0
+        for i in range(numweeks):
+            i_week_breaks.append(prevstop+2+i+1)
+            prevstop+=3+i
+
+        #Format row 3 with thick upper lines and thick start stop week breaks. Thin lower line
+        row3=graphic_range[2]
+        for i in range(len(row3)):
+            if i==0:
+                row3[i].border=openpyxl.styles.Border(left=thickblack, top= thickblack, bottom= thinblack)
+            elif i==output_width-1:
+                row3[i].border = openpyxl.styles.Border(right=thickblack, top=thickblack, bottom=thinblack)
+            elif i in i_week_breaks:
+                row3[i].border = openpyxl.styles.Border(left=thickblack, top=thickblack, bottom=thinblack)
+            elif i+1 in i_week_breaks:
+                row3[i].border = openpyxl.styles.Border(right=thickblack, top=thickblack, bottom=thinblack)
+            else:
+                row3[i].border = openpyxl.styles.Border(top=thickblack, bottom=thinblack)
+
+        # format las row ith Thick bottom lin and thin top line otherwise Thick week breaks.
+        rowlast=graphic_range[output_height-1]
+        for i in range(len(rowlast)):
+            if i==0:
+                rowlast[i].border=openpyxl.styles.Border(left=thickblack, top= thinblack, bottom= thickblack)
+            elif i==output_width-1:
+                rowlast[i].border = openpyxl.styles.Border(right=thickblack, top=thinblack, bottom=thickblack)
+            elif i in i_week_breaks:
+                rowlast[i].border = openpyxl.styles.Border(left=thickblack, top=thinblack, bottom=thickblack)
+            elif i+1 in i_week_breaks:
+                rowlast[i].border = openpyxl.styles.Border(right=thickblack, top=thinblack, bottom=thickblack)
+            else:
+                rowlast[i].border = openpyxl.styles.Border(top=thinblack, bottom=thickblack)
+
+        # Format all interim lines all thin upper and lower. Thick start, stop, weekbreak.
+        for j in range(3,output_height-1):
+            for i in range(output_width):
+                if i == 0:
+                    graphic_range[j][i].border = openpyxl.styles.Border(left=thickblack, top=thinblack, bottom=thinblack)
+                elif i == output_width - 1:
+                    graphic_range[j][i].border = openpyxl.styles.Border(right=thickblack, top=thinblack, bottom=thinblack)
+                elif i in i_week_breaks:
+                    graphic_range[j][i].border = openpyxl.styles.Border(left=thickblack, top=thinblack, bottom=thinblack)
+                elif i + 1 in i_week_breaks:
+                    graphic_range[j][i].border = openpyxl.styles.Border(right=thickblack, top=thinblack, bottom=thinblack)
+                else:
+                    graphic_range[j][i].border = openpyxl.styles.Border(top=thinblack, bottom=thinblack)
 
 
     def writeout(self):
